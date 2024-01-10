1a9b304873ca325498df2820c3f6b5cfafafad37
George Sofroniou <riser.burrs.0c@icloud.com>
2024-01-10 15:57:21 +0000

    G-20240110 JSX - Javascript

--- react-tutorial-app/src/index.js
+++ react-tutorial-app/src/index.js
@@ -13,20 +13,33 @@ function BookList() {
         </section>
     );
 }
-  
-const Book = () => {
-    return <article className='book'>
-        <Image />
-        <Title />
-        <Author />
-    </article>
-};
 
-const Image = () => <img src="./images/book1-atomic_habits-james_clear.jpg" alt="Atomic Habits"/>;
-const Title = () => <h2>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h2>;
-const Author = () => {
-    return <h4>James Clear</h4>;
+const Book = () => {
+    const title = 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones';
+    const author = 'James Clear';
+
+    return (
+        <article className='book'>
+            <img src='./images/book1-atomic_habits-james_clear.jpg' alt='Atomic Habits'/>
+            <h2>{title}</h2>
+            <h4>{author.toUpperCase()}</h4>
+        </article>
+    );
 };
+  
+// const Book = () => {
+//     return <article className='book'>
+//         <Image />
+//         <Title />
+//         <Author />
+//     </article>
+// };
+
+// const Image = () => <img src="./images/book1-atomic_habits-james_clear.jpg" alt="Atomic Habits"/>;
+// const Title = () => <h2>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h2>;
+// const Author = () => {
+//     return <h4>James Clear</h4>;
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
