2ed86ad4c95ec2bf6a2af6caf9fd28d5186efcda
George Sofroniou <riser.burrs.0c@icloud.com>
2024-01-10 15:25:22 +0000

    G-20240110 booklist (1/n)

--- react-tutorial-app/src/index.js
+++ react-tutorial-app/src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
+import './index.css';
+
+function BookList() {
     return (
-        <div>
-            <Person />
-            <Message />
-        </div>
+        <section className='book-list'>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
     );
 }
   
-const Person = () => <h2>Welcome John Doe</h2>;
-const Message = () => {
-    return <p>This is my first component message with nested components.</p>;
+const Book = () => {
+    return <article className='book'>
+        <Image />
+        <Title />
+        <Author />
+    </article>
+};
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg" alt="Atomic Habits"/>;
+const Title = () => <h2>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h2>;
+const Author = () => {
+    return <h4>James Clear</h4>
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
