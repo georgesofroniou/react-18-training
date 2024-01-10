4d5e6a6597f3294d517ba91fb2dd7816bdcffcba
George Sofroniou <riser.burrs.0c@icloud.com>
2024-01-10 16:26:52 +0000

    G-20240110 Props Dynamic (kinda)

--- react-tutorial-app/src/index.js
+++ react-tutorial-app/src/index.js
@@ -2,18 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-const title = 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones';
-const alt = 'Atomic Habits';
-const author = 'James Clear';
-const img = './images/book1-atomic_habits-james_clear.jpg';
+
+// const title = 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones';
+// const alt = 'Atomic Habits';
+// const author = 'James Clear';
+// const img = './images/book1-atomic_habits-james_clear.jpg';
+
+const firstBook = {
+    author:'James Clear',
+    title:'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    img:'./images/book1-atomic_habits-james_clear.jpg',
+    alt:'Atomic Habits',
+};
+const secondBook = {
+    author: 'Rick Rubin',
+    title: 'The Creative Act: A Way of Being',
+    img: './images/book2-the_creative_act-rick_rubin.jpg',
+    alt: 'The Creative Act',
+};
 
 function BookList() {
     return (
         <section className='book-list'>
-            <Book author={author} title={title} img={img} alt={alt} />
-            <Book author={author} title={title} img={img} />
-            <Book author={author} title={title} img={img} />
-            <Book author={author} title={title} img={img} />
+            <Book 
+                author={firstBook.author}
+                title={firstBook.title}
+                img={firstBook.img}
+                alt={firstBook.alt} 
+            />
+            <Book 
+                author={secondBook.author}
+                title={secondBook.title}
+                img={secondBook.img}
+                alt={secondBook.alt}
+            />
         </section>
     );
 }
