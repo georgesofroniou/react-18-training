02913d927a397e216b36bcc3a9e67b4cf3b93a24
George Sofroniou <riser.burrs.0c@icloud.com>
2024-01-10 16:12:55 +0000

    G-20240110 Props

--- react-tutorial-app/src/index.js
+++ react-tutorial-app/src/index.js
@@ -2,31 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+const title = 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones';
+const alt = 'Atomic Habits';
+const author = 'James Clear';
+const img = './images/book1-atomic_habits-james_clear.jpg';
 
 function BookList() {
     return (
         <section className='book-list'>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book author={author} title={title} img={img} alt={alt} />
+            <Book author={author} title={title} img={img} />
+            <Book author={author} title={title} img={img} />
+            <Book author={author} title={title} img={img} />
         </section>
     );
 }
 
-const Book = () => {
-    const title = 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones';
-    const author = 'James Clear';
-    const img = './images/book1-atomic_habits-james_clear.jpg';
-
+const Book = (props) => {
+    console.log(props);
     return (
         <article className='book'>
-            <img src={img} alt={title}/>
-            <h2>{title}</h2>
-            <h4>{author.toUpperCase()}</h4>
+            <img src={props.img} alt={props.alt} />
+            <h2>{props.title}</h2>
+            <h4>{props.author} </h4>
         </article>
     );
 };
+
+// const Book = () => {
+    
+//     return (
+//         <article className='book'>
+//             <img src={img} alt={alt}/>
+//             <h2>{title}</h2>
+//             <h4>{author.toUpperCase()}</h4>
+//         </article>
+//     );
+
+// };
   
 // const Book = () => {
 //     return <article className='book'>
