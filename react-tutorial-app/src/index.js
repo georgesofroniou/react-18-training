495d94d2246f7853a2c331024a6e74dd6891813f
George Sofroniou <riser.burrs.0c@icloud.com>
2024-01-17 14:31:14 +0000

    G-20240117 Events - Fundamentals

--- react-tutorial-app/src/index.js
+++ react-tutorial-app/src/index.js
@@ -25,12 +25,39 @@ const books = [
 const BookList = () => {
   return (
     <section className='booklist'>
+    <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 }
+
+const EventExamples = () => {
+
+  const handleFormInput = () => {
+    console.log('handle form input');
+  };
+
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+  
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name='example'
+          onChange={handleFormInput}
+          style={{margin: '1rem 0'}} 
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>  
+    </section>
+  );
+};
 
 const Book = (props) => {
 
@@ -46,6 +73,7 @@ const Book = (props) => {
     </article>
   );
 };
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
